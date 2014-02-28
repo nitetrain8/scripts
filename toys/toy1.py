ad08c764010bf487c66c70a9a7497e5663e5eec9
nitetrain8 <nathan.starkweather@gmail.com>
2014-02-27 19:23:20 -0800

    decent start to unittesting in foods. Currently learning how to make testing setup not be bad. # 2

--- toys/toy1.py
+++ toys/toy1.py
@@ -10,33 +10,3 @@ Created in: PyCharm Community Edition
 __author__ = 'Nathan Starkweather'
 
 
-# mystr = __author__
-# # print(bool('0'))
-# from itertools import takewhile
-# mylist = [1, 2, 3, 4, 5, None, 6, 7]
-
-
-from pysrc.snippets import printdir
-
-
-# import collections.abc
-#
-#
-# class myclass():
-#     def __getitem__(self, item):
-#         print("foo")
-#         print(item)
-#         printdir(item)
-#         print(isinstance(item, slice))
-#
-#
-# def main():
-#     m = myclass()[::2]
-#     return m
-# main()
-
-from os import listdir
-
-from os.path import dirname
-
-"foo".replace('o', 'i')
