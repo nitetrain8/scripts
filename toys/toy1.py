03737cbc7966d3fd614272f116a0d9df5e771909
nitetrain8 <nathan.starkweather@gmail.com>
2014-02-27 21:49:41 -0800

    updates to mock strptime. speed improved ~50% by removing call to _getlang()

--- toys/toy1.py
+++ toys/toy1.py
@@ -9,4 +9,453 @@ Created in: PyCharm Community Edition
 
 __author__ = 'Nathan Starkweather'
 
+from time import perf_counter as _timer
 
+
+def test_if(loops):
+    timer = _timer
+    times = []
+    foo_dict = {}
+    foo_key = 'foo key'
+    for i in range(loops):
+        t1 = timer()
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        foo_result = foo_dict.get(foo_key)
+        if foo_result: pass
+        times.append(timer()-t1)
+    return sum(times) / len(times)
+
+
+def test_raise(loops):
+    timer = _timer
+    times = []
+    foo_dict = {}
+    foo_key = 'foo key'
+    for i in range(loops):
+        t1 = timer()
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        try: foo_dict[foo_key]
+        except KeyError: pass
+        times.append(timer() - t1)
+    return sum(times) / len(times)
+
+
+if __name__ == '__main__':
+
+    loops = 100000
+    ifs = test_if(loops)
+    raises = test_raise(loops)
+    print("If test:", ifs * 10e5)
+    print("Raise test:", raises * 10e5)
