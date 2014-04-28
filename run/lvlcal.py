d8cadec7f2c6ac4d869d684182c873f716b7fd51
nitetrain8 <nathan.starkweather@gmail.com>
2014-04-28 13:04:13 -0700

    misc

--- run/lvlcal.py
+++ run/lvlcal.py
@@ -34,14 +34,39 @@ class LvlCal():
     def zero(self, x):
         self.b -= x
 
-    def span(self, x, y):
-        x1 = - self.b / self.m
+    def zero2(self, x):
+        x2 = x + 1
+        y2 = self.calcy(x2)
 
-        newm = y / (x - x1)
-        newb = y - newm * x
+        m, b = calc_regression((x, 0), (x2, y2))
+        self.m = m
+        self.b = b
+
+    def span(self, x, y):
+        """
+        Span is performed by "rotating" the calibration around
+        the zero point, ie the point at which y = 0 (x intercept).
+
+        The new m and b should be calculated by finding the regression
+        equation between the new (x, y) coordinate, and the x intercept,
+        which is given by (-b/m, 0).
+
+        @param x: x
+        @type x: float | int
+        @param y: y
+        @type y: float | int
+        @return: None
+        @rtype: None
+        """
+
+        xint = (-self.b / self.m, 0)
+        newm, newb = calc_regression(xint, (x, y))
 
         self.m = newm
         self.b = newb
+
+    def calcy(self, x):
+        return self.m * x + self.b
 
     def __repr__(self):
         return "m: %.2f b: %.2f" % (self.m, self.b)
